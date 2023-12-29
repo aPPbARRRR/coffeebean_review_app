a81f0f88d4593bd9f8b68210399c5238b6d9c1c3
jun <134025007+aPPbARRRR@users.noreply.github.com>
2023-12-29 12:28:35 +0900

    아키텍처 기본 템플릿 적용 및 파베 연동

---
+++ lib/mask/presentation/widget/store_container.dart
@@ -0,0 +1,43 @@
+// import 'package:flutter/material.dart';
+
+// import '../../business/model/store.dart';
+
+// class StoreContainer extends StatelessWidget {
+//   const StoreContainer({
+//     Key? key,
+//     required this.store,
+//   }) : super(key: key);
+//   final Store store;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return IntrinsicHeight(
+//       child: Container(
+//         color: Colors.amber,
+//         child: Padding(
+//           padding: const EdgeInsets.all(8.0),
+//           child: Row(children: [
+//             Expanded(
+//               flex: 7,
+//               child: Column(
+//                 children: [
+//                   Text(store.name),
+//                   Text(store.addr),
+//                   Text(
+//                     '거리. lat, lang 이용해서 구할 것.\n코어 - 유틸에서 구하는 로직 만들고 유즈케이스에서 활용할 것',
+//                   )
+//                 ],
+//               ),
+//             ),
+//             Expanded(
+//               flex: 3,
+//               child: Column(
+//                 children: [Text('충분'), Text(store.remainStat)],
+//               ),
+//             )
+//           ]),
+//         ),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/mask/presentation/widget/stores_list_view.dart
@@ -0,0 +1,27 @@
+// // ignore_for_file: public_member_api_docs, sort_constructors_first
+// import 'package:flutter/material.dart';
+// import 'package:mask_store_app/mask/presentation/widget/store_container.dart';
+
+// import '../../business/model/store.dart';
+
+// class StoresListView extends StatelessWidget {
+//   const StoresListView({
+//     Key? key,
+//     required this.stores,
+//   }) : super(key: key);
+//   final List<Store> stores;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView(
+//       children: [
+//         ...stores
+//             .map((store) => Padding(
+//                   padding: EdgeInsets.all(10),
+//                   child: StoreContainer(store: store),
+//                 ))
+//             .toList()
+//       ],
+//     );
+//   }
+// }
