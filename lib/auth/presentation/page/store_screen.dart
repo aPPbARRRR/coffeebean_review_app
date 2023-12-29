760db78be2b271a0e024ce5ac23923905d8b7f41
jun <134025007+aPPbARRRR@users.noreply.github.com>
2023-12-29 12:40:28 +0900

    리버팟 pub add

---
+++ lib/auth/presentation/page/store_screen.dart
@@ -0,0 +1,53 @@
+// import 'package:flutter/material.dart';
+// import 'package:mask_store_app/mask/presentation/view_model/stores_view_model.dart';
+// import 'package:mask_store_app/mask/presentation/widget/stores_list_view.dart';
+
+// import '../../business/model/store.dart';
+
+// class StoreScreen extends StatefulWidget {
+//   const StoreScreen({super.key});
+
+//   @override
+//   State<StoreScreen> createState() => _StoreScreenState();
+// }
+
+// class _StoreScreenState extends State<StoreScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final StoresViewModel viewModel = StoresViewModel();
+
+//     return FutureBuilder(
+//         future: viewModel.fetchData(),
+//         builder: (context, snapShot) {
+//           if (snapShot.connectionState != ConnectionState.done)
+//             return Scaffold(
+//               body: Center(
+//                 child: CircularProgressIndicator(),
+//               ),
+//             );
+
+//           return Scaffold(
+//             appBar: AppBar(
+//               title: Text(
+//                   '마스크 재고 있는 곳 : ${viewModel.stores.where((store) => store.remainStat != 'break').length} 개소'),
+//             ),
+//             body: viewModel.stores.length != 0
+//                 ? Column(
+//                     children: [
+//                       Expanded(
+//                         child: RefreshIndicator(
+//                             onRefresh: () async {
+//                               await viewModel.fetchData();
+//                               setState(() {});
+//                             },
+//                             child: StoresListView(stores: viewModel.stores)),
+//                       ),
+//                     ],
+//                   )
+//                 : Center(
+//                     child: Text('검색결과가 없습니다.'),
+//                   ),
+//           );
+//         });
+//   }
+// }
